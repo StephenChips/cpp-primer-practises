27c20157fc33451380781a884c76953f2a408662
StephenChips <tcw6991@gmail.com>
2021-08-22 15:25:51 +0800

    save

--- chapter-9/9.27.cpp
+++ chapter-9/9.27.cpp
@@ -1,39 +1,11 @@
-<<<<<<< HEAD
-#include <iostream>
-#include <forward_list>
-=======
 #include <forward_list>
 #include <iostream>
 #include <iterator>
->>>>>>> f8853916b656477f732d054877d923bbd1e0e7c8
 
 int main()
 {
     std::forward_list<int> list{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-<<<<<<< HEAD
-    auto it = list.begin();
-    auto itBefore = list.before_begin();
-    
-    while (it != list.end())
-    {
-        if ((*it & 1) == 1)
-        {
-            list.erase_after(itBefore);
-            it = std::next(itBefore);
-        }
-        else
-        {
-            it++;
-            itBefore++;
-        }
-    }
-
-    for (int i : list)
-    {
-        std::cout << i << " ";
-    }
-=======
     auto bi = list.before_begin();
 
     while (std::next(bi) != list.end())
@@ -52,7 +24,6 @@ int main()
         std::cout << i << " ";
     }
 
->>>>>>> f8853916b656477f732d054877d923bbd1e0e7c8
     std::cout << std::endl;
 
     return 0;
--- chapter-9/9.28.cpp
+++ chapter-9/9.28.cpp
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-#include <string>
-#include <forward_list>
-#include <iostream>
-
-void find_and_insert(std::forward_list<std::string> &list, const std::string &target, const std::string &newWord);
-
-void find_and_insert(std::forward_list<std::string> &list, const std::string &target, const std::string &newWord)
-{
-    auto iter_prev = list.before_begin();
-    auto iter = list.begin();
-
-    if (iter == list.end())
-    {
-        list.insert_after(iter_prev, newWord);
-        return;
-    }
-
-    while (iter != list.end())
-    {
-        if (*iter == target)
-        {
-            list.insert_after(iter, newWord);
-            return;
-        }
-        iter_prev++;
-        iter++;
-    }
-
-    list.insert_after(iter_prev, newWord);
-=======
 #include <iostream>
 #include <forward_list>
 #include <string>
@@ -55,23 +24,10 @@ void insert_after(forward_list<string> &list, const string &s1, const string &s2
         before_begin++;
     }
     list.insert_after(before_begin, s2);
->>>>>>> f8853916b656477f732d054877d923bbd1e0e7c8
 }
 
 int main()
 {
-<<<<<<< HEAD
-    std::forward_list<std::string> list{};
-
-    find_and_insert(list, "graduate", "school");
-    find_and_insert(list, "school", "graduate");
-    find_and_insert(list, "student", "student");
-
-    for (std::string &str : list)
-    {
-        std::cout << str << std::endl;
-    }
-=======
     forward_list<string> list{
         "hello",
         "world",
@@ -85,5 +41,4 @@ int main()
     }
 
     cout << endl;
->>>>>>> f8853916b656477f732d054877d923bbd1e0e7c8
 }
